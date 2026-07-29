8ab6af0116483fefaedea3d9139cc83833b7b4fb
Andreaara <20243ds171@utez.edu.mx>
2026-07-29 12:48:58 -0600

    cambios

--- src/main/java/mx/edu/utez/JuventudxTemixco/service/CredencializacionService/Credencialización.java
+++ src/main/java/mx/edu/utez/JuventudxTemixco/service/CredencializacionService/Credencialización.java
@@ -1,14 +1,12 @@
 package mx.edu.utez.JuventudxTemixco.service.CredencializacionService;
 
 import net.sf.jasperreports.engine.*;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.util.List;
 import java.util.Map;
 
 @Service
@@ -25,12 +23,18 @@ public class Credencialización {
         InputStream reporteStream = getClass().getResourceAsStream(ruta);
 
         if (reporteStream == null) {
-            throw new RuntimeException("No se encontro la plantilla del Reporte");
+            throw new RuntimeException("No se encontró la plantilla del Reporte en la ruta: " + ruta);
         }
 
         InputStream logoStream = getClass().getResourceAsStream("/img/logo.png");
+        if (logoStream == null) {
+            logoStream = getClass().getResourceAsStream("/static/img/logo.png");
+        }
+
         if (logoStream != null) {
             parametros.put("LOGO_LOGO", logoStream);
+        } else {
+            System.out.println("ADVERTENCIA: No se encontró el logo en /img/logo.png ni en /static/img/logo.png");
         }
 
         JasperReport reporte = JasperCompileManager.compileReport(reporteStream);
@@ -44,6 +48,4 @@ public class Credencialización {
             return JasperExportManager.exportReportToPdf(print);
         }
     }
-
-
 }
